17b10a9e932e406bc661863126034e15295c6a1d
woojiahao <woojiahao1234@gmail.com>
2021-12-03 14:05:39 +0800

    Add day 3 part 2

--- day-3/solution.exs
+++ day-3/solution.exs
@@ -5,16 +5,19 @@ defmodule DayThreeSolution do
     |> Enum.map(fn x -> String.split(x, "", trim: true) end)
   end
 
-  defp highest(data, i) do
+  defp common(data, i, sorter \\ fn a, b -> a >= b end) do
     data
-    |> Enum.map(fn x -> Enum.at(x, i) end)
-    |> Enum.frequencies()
-    |> Enum.max_by(fn {_, v} -> v end)
+    |> Enum.frequencies_by(fn x -> Enum.at(x, i) end)
+    |> IO.inspect()
+    |> Enum.max_by(fn {_, v} -> v end, sorter)
     |> elem(0)
   end
 
   defp flip("0"), do: "1"
   defp flip("1"), do: "0"
+
+  defp to_integer(binary_string),
+    do: binary_string |> Enum.join("") |> Integer.parse(2) |> elem(0)
 
   defp solve(data) do
     iters = length(Enum.at(data, 0))
@@ -22,13 +25,36 @@ defmodule DayThreeSolution do
     highest =
       0..(iters - 1)
       |> Enum.to_list()
-      |> Enum.map(fn i -> highest(data, i) end)
+      |> Enum.map(fn i -> common(data, i) end)
 
-    gamma = highest |> Enum.join("") |> Integer.parse(2) |> elem(0)
-    epsilon = highest |> Enum.map(&flip/1) |> Enum.join("") |> Integer.parse(2) |> elem(0)
+    gamma = highest |> to_integer()
+    epsilon = highest |> Enum.map(&flip/1) |> to_integer()
 
     gamma * epsilon
   end
 
+  defp oxygen([last], _), do: last |> to_integer()
+
+  defp oxygen(data, i) do
+    highest = data |> common(i, fn a, b -> a > b end)
+    filtered = data |> Enum.filter(fn x -> Enum.at(x, i) == highest end)
+    oxygen(filtered, i + 1)
+  end
+
+  defp carbon_dioxide([last], _), do: last |> to_integer()
+
+  defp carbon_dioxide(data, i) do
+    lowest = data |> common(i, fn a, b -> b >= a end) |> IO.inspect()
+    filtered = data |> Enum.filter(fn x -> Enum.at(x, i) == lowest end) |> IO.inspect()
+    carbon_dioxide(filtered, i + 1)
+  end
+
+  defp solve_two(data) do
+    o = oxygen(data, 0)
+    co = carbon_dioxide(data, 0)
+    o * co
+  end
+
   def part_one(), do: load_data() |> solve()
+  def part_two(), do: load_data() |> solve_two()
 end
