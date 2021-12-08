1a2c6d227110f1e55733811e248ea349f196de1f
woojiahao <woojiahao1234@gmail.com>
2021-12-08 13:25:54 +0800

    Update day 7 solution

--- lib/advent_of_code/day-7/solution.ex
+++ lib/advent_of_code/day-7/solution.ex
@@ -7,17 +7,35 @@ defmodule AdventOfCode.DaySevenSolution do
       |> Enum.map(&String.to_integer/1)
 
   defp solve(data) do
-    data
-    |> Enum.map(fn pt ->
-      total_fuel =
-        data
-        |> Enum.map(fn other -> abs(other - pt) end)
-        |> Enum.sum()
+    {min, max} = data |> Enum.min_max()
 
-      {pt, total_fuel}
+    min..max
+    |> Enum.map(fn goal ->
+      data
+      |> Enum.map(fn pt -> abs(goal - pt) end)
+      |> Enum.sum()
     end)
-    |> Enum.min_by(&elem(&1, 1))
+    |> Enum.min()
   end
 
   def part_one(), do: load_data() |> solve()
+
+  defp calculate_expensive_fuel(pt, other) do
+    x = abs(pt - other)
+    trunc(0.5 * x * x + 0.5 * x)
+  end
+
+  defp expensive_solve(data) do
+    {min, max} = data |> Enum.min_max()
+
+    min..max
+    |> Enum.map(fn goal ->
+      data
+      |> Enum.map(&calculate_expensive_fuel(&1, goal))
+      |> Enum.sum()
+    end)
+    |> Enum.min()
+  end
+
+  def part_two(), do: load_data() |> expensive_solve()
 end
