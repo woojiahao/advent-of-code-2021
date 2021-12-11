1eb3155a3a84cac6d7eec834cefd395e15bacb89
woojiahao <woojiahao1234@gmail.com>
2021-12-11 15:56:30 +0800

    Modify day 9 to use graph instead

--- lib/advent_of_code/day-9/solution.ex
+++ lib/advent_of_code/day-9/solution.ex
@@ -1,4 +1,10 @@
+defmodule AdventOfCode.DayNineSolution.Location do
+  defstruct [:coords, :height]
+end
+
 defmodule AdventOfCode.DayNineSolution do
+  alias AdventOfCode.DayNineSolution.Location, as: Location
+
   # Alt solution is to manually check coords
   defp load_data() do
     m =
@@ -7,59 +13,50 @@ defmodule AdventOfCode.DayNineSolution do
       |> Enum.map(fn i -> Enum.map(i, &String.to_integer/1) end)
       |> Matrex.new()
 
-    {m, Matrex.size(m)}
+    {max_r, max_c} = Matrex.size(m)
+
+    gen_graph(m, 1, 1, max_r, max_c, :digraph.new(), [])
   end
 
-  # r and c represent the coords of the center point
-  defp get_submatrix_size(r, c, max_r, max_c) do
-    r_start = if r == 1, do: 1, else: r - 1
-    r_end = if r == max_r, do: r, else: r + 1
-    c_start = if c == 1, do: 1, else: c - 1
-    c_end = if c == max_c, do: c, else: c + 1
-    {r_start..r_end, c_start..c_end}
+  defp find_pt(pts, r, c),
+    do: pts |> Enum.find(fn %{coords: {row, col}} -> row == r and col == c end)
+
+  defp gen_graph(_, r, _, max_r, _, g, pts) when r > max_r, do: {g, pts}
+
+  defp gen_graph(m, r, c, max_r, max_c, g, pts) when c > max_c,
+    do: gen_graph(m, r + 1, 1, max_r, max_c, g, pts)
+
+  defp gen_graph(m, r, c, max_r, max_c, g, pts) do
+    up = if r == 1, do: nil, else: {r - 1, c}
+    down = if r == max_r, do: nil, else: {r + 1, c}
+    left = if c == 1, do: nil, else: {r, c - 1}
+    right = if c == max_c, do: nil, else: {r, c + 1}
+
+    cur = %Location{coords: {r, c}, height: m[r][c]}
+    :digraph.add_vertex(g, cur)
+
+    [up, down, left, right]
+    |> Enum.filter(&(&1 != nil))
+    |> Enum.map(fn {row, col} ->
+      pt = %Location{coords: {row, col}, height: m[row][col]}
+      :digraph.add_vertex(g, pt)
+      :digraph.add_edge(g, cur, pt)
+    end)
+
+    gen_graph(m, r, c + 1, max_r, max_c, g, [cur | pts])
   end
 
-  defp acc(data, acc_type \\ :value), do: acc(data, 1, 1, [], acc_type)
+  def part_one() do
+    {g, pts} = load_data()
 
-  defp acc({_, {max_r, _}}, r, _, acc, _) when r > max_r, do: acc
-
-  defp acc({m, {max_r, max_c}}, r, c, acc, acc_type) when c > max_c,
-    do: acc({m, {max_r, max_c}}, r + 1, 1, acc, acc_type)
-
-  # Get from the sub-matrix
-  defp acc({m, {max_r, max_c}}, r, c, acc, acc_type) do
-    {rows, cols} = get_submatrix_size(r, c, max_r, max_c)
-    sm = m |> Matrex.submatrix(rows, cols)
-    center = m |> Matrex.at(r, c)
-    sm_min = sm |> Matrex.min()
-    is_center? = sm_min == center
-    is_unique? = sm |> Enum.count(&(&1 == sm_min)) == 1
-
-    v =
-      case acc_type do
-        :coords -> {r, c}
-        :value -> center
-        _ -> raise "invalid acc_type"
-      end
-
-    u_acc = if is_center? and is_unique?, do: [v], else: []
-
-    acc({m, {max_r, max_c}}, r, c + 1, acc ++ u_acc, acc_type)
-  end
-
-  def part_one(),
-    do:
-      load_data()
-      |> acc()
-      |> Enum.map(&(&1 + 1))
-      |> Enum.sum()
-
-  # generate a graph of the adjacent nodes
-  defp find_basin_dim(m, r, c, max_r, max_c) do
+    pts
+    |> Enum.filter(fn pt ->
+      :digraph.out_neighbours(g, pt) |> Enum.all?(fn %{height: h} -> h > pt.height end)
+    end)
+    |> Enum.map(fn %{height: h} -> h + 1 end)
+    |> Enum.sum()
   end
 
   def part_two(),
-    do:
-      load_data()
-      |> acc(:coords)
+    do: load_data()
 end
