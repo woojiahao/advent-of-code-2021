e11930c675a7f084977634edd8ffc171250f948c
woojiahao <woojiahao1234@gmail.com>
2021-12-17 00:42:38 +0800

    Add day 12 part 1

---
+++ lib/advent_of_code/day-12/solution.ex
@@ -0,0 +1,57 @@
+defmodule AdventOfCode.DayTwelveSolution do
+  defp load_data() do
+    map =
+      AdventOfCode.load_data(12, "data.txt")
+      |> Enum.map(&String.split(&1, "-"))
+
+    g = :digraph.new()
+
+    map
+    |> Enum.each(fn [s, e] ->
+      as = String.to_atom(s)
+      ae = String.to_atom(e)
+      :digraph.add_vertex(g, as)
+      :digraph.add_vertex(g, ae)
+      :digraph.add_edge(g, as, ae)
+      :digraph.add_edge(g, ae, as)
+    end)
+
+    g
+  end
+
+  defp walk(_, :end, _, path), do: [path]
+
+  defp walk(g, cur, vs, path) do
+    available = (:digraph.out_neighbours(g, cur) -- [:start]) -- vs
+
+    available
+    |> Enum.map(fn a ->
+      u_vs = if Atom.to_string(a) == String.downcase(Atom.to_string(a)), do: vs ++ [a], else: vs
+      walk(g, a, u_vs, path ++ [a])
+    end)
+    |> Enum.flat_map(& &1)
+  end
+
+  defp walk_two_visits(_, :end, _, path), do: [path]
+
+  defp walk_two_visits(g, cur, vs, path) do
+    # visited will be filled with all paths if there is a small cave with 2
+    visited = if Enum.any?(Map.values(vs), &(&1 == 2)), do: Map.values(vs), else: []
+    available = (:digraph.out_neighbours(g, cur) -- [:start]) -- visited
+
+    available
+    |> Enum.map(fn a ->
+      u_vs = if Atom.to_string(a) == String.downcase(Atom.to_string(a)), do: vs ++ [a], else: vs
+      walk(g, a, u_vs, path ++ [a])
+    end)
+    |> Enum.flat_map(& &1)
+  end
+
+  def part_one() do
+    load_data() |> walk(:start, [], [:start]) |> length()
+  end
+
+  def part_two() do
+    load_data() |> walk_two_visits(:start, %{}, [:start]) |> length()
+  end
+end
