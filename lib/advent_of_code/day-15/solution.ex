99c57d6b612529d2432c236820914947008f3bfd
woojiahao <woojiahao1234@gmail.com>
2021-12-17 22:16:47 +0800

    Add day 15 part 2

--- lib/advent_of_code/day-15/solution.ex
+++ lib/advent_of_code/day-15/solution.ex
@@ -1,6 +1,4 @@
 defmodule AdventOfCode.DayFifteenSolution do
-  @maze_size 100
-
   defp load_data() do
     AdventOfCode.load_data(15, "data.txt")
     |> Enum.map(&String.graphemes/1)
@@ -8,11 +6,11 @@ defmodule AdventOfCode.DayFifteenSolution do
     |> Matrex.new()
   end
 
-  defp in_grid(row, col), do: row >= 1 and col >= 1 and row <= @maze_size and col <= @maze_size
+  defp in_grid(row, col, dim), do: row >= 1 and col >= 1 and row <= dim and col <= dim
 
-  defp dijkstra(m, costs, unvisited) do
+  defp dijkstra(m, costs, unvisited, dim) do
     if :queue.len(unvisited) == 0 do
-      costs[@maze_size][@maze_size] - 1.0
+      costs[dim][dim] - 1.0
     else
       {{:value, {{row, col}, cost}}, u_unvisited} = :queue.out(unvisited)
 
@@ -23,7 +21,7 @@ defmodule AdventOfCode.DayFifteenSolution do
           {row, col - 1},
           {row, col + 1}
         ]
-        |> Enum.filter(fn {ar, ac} -> in_grid(ar, ac) end)
+        |> Enum.filter(fn {ar, ac} -> in_grid(ar, ac, dim) end)
         |> Enum.filter(fn {ar, ac} -> costs[ar][ac] > cost + m[ar][ac] end)
 
       updated_costs =
@@ -37,7 +35,7 @@ defmodule AdventOfCode.DayFifteenSolution do
         end)
         |> prioritize_queue()
 
-      dijkstra(m, updated_costs, updated_unvisited)
+      dijkstra(m, updated_costs, updated_unvisited, dim)
     end
   end
 
@@ -48,10 +46,36 @@ defmodule AdventOfCode.DayFifteenSolution do
     |> Enum.reduce(:queue.new(), fn el, acc -> :queue.in(el, acc) end)
   end
 
+  defp solve(m) do
+    {dim, _} = Matrex.size(m)
+    costs = Matrex.new(dim, dim, fn -> 9_9999 end) |> Matrex.set(1, 1, m[1][1])
+    unvisited = :queue.new() |> then(fn q -> :queue.in({{1, 1}, m[1][1]}, q) end)
+    dijkstra(m, costs, unvisited, dim)
+  end
+
   def part_one() do
     m = load_data()
-    costs = Matrex.new(@maze_size, @maze_size, fn -> 9_9999 end) |> Matrex.set(1, 1, m[1][1])
-    unvisited = :queue.new() |> then(fn q -> :queue.in({{1, 1}, m[1][1]}, q) end)
-    dijkstra(m, costs, unvisited)
+    solve(m)
+  end
+
+  def part_two() do
+    m = load_data()
+    {dim, _} = Matrex.size(m)
+    g = for x <- 0..4, y <- 0..4, do: {x, y}
+    i = for row <- 1..dim, col <- 1..dim, do: {row, col}
+
+    n_m =
+      Enum.reduce(g, Matrex.new(dim * 5, dim * 5, fn -> 0 end), fn {row, col}, acc ->
+        IO.puts("#{row}, #{col}")
+        ui = i |> Enum.map(fn {r, c} -> {r + row * dim, c + col * dim} end)
+        IO.inspect(List.first(ui))
+
+        Enum.reduce(ui, acc, fn {r, c}, a ->
+          Matrex.set(a, r, c, m[r - row * dim][c - col * dim] + row + col)
+        end)
+      end)
+      |> Matrex.apply(fn v -> if v > 9, do: v - 9, else: v end)
+
+    solve(n_m)
   end
 end
